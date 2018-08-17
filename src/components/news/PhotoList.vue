6401b4527e74f869584c94cf6820738ec9e1792c
cyr1402123126 <1402123126@qq.com>
2018-08-17 17:51:32 +0800

    '图文分享请求数据'

--- src/components/news/PhotoList.vue
+++ src/components/news/PhotoList.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
       <nav-bar :title="title" :path="path"></nav-bar>
-      <main>
-        <article v-for="item in list">
+      <mt-loadmore  :top-method="loadTop" :bottom-method="loadBottom" @top-status-change="handleTopChange" ref="loadmore" :autoFill="false">
+        <ul>
+          <li v-for="item in list">
+            <h3>{{item.title}}</h3>
+            <img v-lazy="item.src" alt="">
+            <p>{{item.content}}</p>
+          </li>
+        </ul>
+        <div slot="top" class="mint-loadmore-top">
+          <span v-show="topStatus !== 'loading'" :class="{ 'rotate': topStatus === 'drop' }">↓</span>
+          <span v-show="topStatus === 'loading'">Loading...</span>
+        </div>
+      </mt-loadmore>
+     <!-- <ul>
+        <li v-for="item in list">
           <h3>{{item.title}}</h3>
           <img v-lazy="item.src" alt="">
           <p>{{item.content}}</p>
-        </article>
-      </main>
+        </li>
+      </ul>-->
     </div>
 </template>
 
@@ -19,19 +32,22 @@
         title:'图文分享',
         path:'/',
         list:[],
+        topStatus: '',
+        page:1
       }
     },
     created(){
-      this.getData(this.$route.params.categoryId);
+      this.getData(this.$route.params.categoryId,this.page);
     },
     methods:{
       getData(id){
         if (id==0) {
-          this.axios.get('photo').then(res=>{
+          this.axios.get('photo?_page='+this.page+'&_limit=5').then(res=>{
             res.data.forEach(value=>{
               value.src=require('../../assets/images/photo/'+value.src)
             })
-            this.list=res.data;
+            this.page++;
+            this.list=this.list.concat(res.data);
           });
         }else {
           this.axios.get('photo',{params:{id:id}}).then(res=>{
@@ -41,33 +57,43 @@
             this.list=res.data;
           });
         }
-      }
+      },
+      loadTop(){
+        this.$refs.loadmore.onTopLoaded();
+        console.log("函数呗调用了");
+      },
+      loadBottom(){
+        this.getData(this.$route.params.categoryId,this.page);
+        console.log("上啦了");
+      },
+      handleTopChange(status) {
+        this.topStatus = status;
+        console.log(status);
+      },
     }
   }
 </script>
 
 <style scoped>
-  main {
-    margin-top: 40px;
+  ul {
     padding: 15px;
-    margin-bottom: 50px;
   }
-  article {
+  li {
     border: 1px solid #e0e0e0;
     overflow: hidden;
     box-sizing: border-box;
     margin-bottom: 10px;
   }
-  article h3 {
+  li h3 {
     text-align: center;
     margin: 10px 0;
   }
-  article img {
+  li img {
     width: 100%;
     box-shadow: 1px 1px 1px rgb(212, 212, 212);
     box-sizing: border-box;
   }
-  article p {
+  li p {
     font-size: 14px;
     line-height: 2;
     margin-top: 10px;
